b4a231cd56ebfbb8e43e43df9afc6be1b13b1692
Darío Javier Cravero <dario@uxtemple.com>
2021-10-15 17:40:44 +0200

    wip: parcel support

--- src/App/logic.js
+++ src/App/logic.js
@@ -1,8 +1,9 @@
-import { ViewsFlow } from 'Views/Flow.js'
+import { ViewsFlow } from '/src/Views/Flow.js'
 import View from './view.js'
 import React from 'react'
 
 export default function Logic(props) {
+  console.log('testwww')
   return (
     <ViewsFlow>
       <View {...props} />
